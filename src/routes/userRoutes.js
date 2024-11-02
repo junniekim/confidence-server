132f6df32301ff66df299e616254f273b20f6e7e
Sijun Kim <junniekim12@gmail.com>
2024-11-02 16:51:38 -0400

    Prevent SQL injection for signing in

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bcrypt from "bcrypt";
 const router = express.Router();
 import User from "../models/User.js";
 // Route for creating a new account
@@ -13,13 +14,15 @@ router.post("/signup", async (req, res) => {
         "Please provide first Name, last Name, email address, and password.",
     });
   }
-
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  console.log(password, hashedPassword);
   try {
     const newUser = new User({
       firstName,
       lastName,
       emailAddress,
-      password,
+      password: hashedPassword,
       birthday,
       phoneNumber,
     });
@@ -45,23 +48,22 @@ router.post("/signin", async (req, res) => {
   const { emailAddress, password } = req.body;
   console.log(emailAddress, password);
   try {
-    const user = await User.findOne({
-      emailAddress: emailAddress,
-      password: password,
-    }); // Find user
-    if (!user) {
+    const user = await User.findOne({ emailAddress: emailAddress });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      console.log(new Date());
+      console.log("Sign In - Success");
+      res.status(200).json({
+        message: "User located successfully",
+        data: {
+          id: user._id,
+          firstName: user.firstName,
+          lastName: user.lastName,
+        },
+      });
+    } else {
       return res.status(400).json({ error: "Invalid email or password." });
     }
-    console.log(new Date());
-    console.log("Sign In - Success");
-    res.status(200).json({
-      message: "User located successfully",
-      data: {
-        id: user._id,
-        firstName: user.firstName,
-        lastName: user.lastName,
-      },
-    });
   } catch (error) {
     console.error("Error signing in:", error);
     res.status(500).json({ error: "Internal server error" });
